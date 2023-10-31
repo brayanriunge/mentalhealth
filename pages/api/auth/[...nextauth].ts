2c8c4e05b3078494cb3945357a8499fb9fc3a241
RiugeBrian <riungebrayandev@gmail.com>
2023-10-31 12:13:14 +0300

    Creating Auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/util/db";
+import { NextAuthOptions } from "next-auth";
+
+
+
+export const authOptions: NextAuthOptions= ({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // session: {
+  //   strategy: "jwt",
+  // },
+})
+
+export default NextAuth(authOptions)
