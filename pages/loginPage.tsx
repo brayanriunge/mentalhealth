979bc145d02e594a36044314896ddbd1563e6557
RiugeBrian <riungebrayandev@gmail.com>
2023-11-01 18:15:46 +0300

    Creating register Page

--- pages/loginPage.tsx
+++ pages/loginPage.tsx
@@ -1,16 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
+import google from "@/public/Home.jpeg"
+import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
+import { useState } from "react";
 
 export default function Login(){
+    const [show, setShow]=useState(false)
     return(
         <div className="flex h-screen bg-blue-400">
             <div className=" bg-slate-50 m-auto w-3/5 h-3/4 rounded-md">
-                {/* <div className="bg-gradient-to-r from-blue-500 to-indigo-500 rounded-l-md relative overflow-hidden  ">
-                    <div className="bg-no-repeat bottom-0 left-0 right-0 top-0 bg-cover absolute"
-                     style={{backgroundImage: "url(/login.jpeg)",
-                     width: "1"
-                    }}
-                    ></div>
-                </div> */}
                 {/* Login form */}
                 <div className=" flex flex-col text-center gap-10 h-full rounded-md">
                     <div className="m-auto px-4 py-4">
@@ -23,22 +21,35 @@ export default function Login(){
                           type="email"
                           name="email"
                           placeholder="Email"
-                          className="w-full px-6 py-4 rounded-xl bg-slate-50 focus:outline-none border-none"
+                          className="w-full px-6 py-4 rounded-xl bg-slate-50 focus:outline-none border-none "
                          />
+                         <span className="icon flex items-center px-4 ">
+                            <HiAtSymbol className="h-[25px] w-[25px]" />
+                         </span>
                         </div>
-                        <div className="flex border border-gray-400  rounded-md relative">
+                        <div className="flex border border-gray-400  rounded-md relative ">
                          <input
-                          type="password"
+                          type={`${show ? "text":"password"}`}
                           name="password"
                           placeholder="Password"
                           className="w-full px-6 py-4 rounded-xl bg-slate-50 focus:outline-none border-none"
                          />
+                         <span className="icon flex items-center px-4  " onClick={()=> setShow(!show)}>
+                            <HiFingerPrint className="h-[25px] w-[25px]" />
+                         </span>
                         </div>
                         <div className="">
-                            <button type="submit" className="w-full bg-gradient-to-r from-blue-500 rounded-md to-indigo-500 py-3 text-gray-50 text-lg"> Login</button>
+                            <button type="submit" className="w-full bg-gradient-to-r from-blue-500 rounded-md to-indigo-500 py-3 text-gray-50 text-lg
+                            hover:bg-gradient-to-r  hover:from-gray-50 hover:to-gray-100 hover:border-blue-500 hover:text-gray-700 hover:border"
+                            >
+                             Login
+                            </button>
                         </div>
                         <div className="">
-                            <button type="submit" className=""> Sign in with Google</button>
+                            <button type="button" className="w-full py-3 border flex justify-between items-center gap-4  rounded-lg hover:bg-gray-300"> 
+                             <p >Sign in with Google</p>
+                             <Image src={google} height={30} width={30} alt="google" className="rounded-full"/>
+                            </button>
                         </div>
                        </form>
                        <p>
