43961db18b2bdf2f9ce39d3ade632a9275de24c6
RiugeBrian <riungebrayandev@gmail.com>
2023-11-01 16:09:32 +0300

    Creating loginPage

--- pages/loginPage.tsx
+++ pages/loginPage.tsx
@@ -1,16 +1,20 @@
 export default function Login(){
     return(
         <div className="flex h-screen bg-blue-400">
-            <div className="grid grid-cols-2 bg-slate-50 m-auto w-3/5 h-3/4 rounded-md">
-                <div className="bg-gradient-to-r from-blue-500 to-indigo-500 rounded-l-md relative overflow-hidden  ">
-                    <div className="bg-no-repeat bottom-0 left-0 right-0 top-0 bg-cover absolute bg-gradient-to-r from-blue-500 to-indigo-500 s"
-                     style={{backgroundImage: "url(/person.jpeg)"}}
+            <div className=" bg-slate-50 m-auto w-3/5 h-3/4 rounded-md">
+                {/* <div className="bg-gradient-to-r from-blue-500 to-indigo-500 rounded-l-md relative overflow-hidden  ">
+                    <div className="bg-no-repeat bottom-0 left-0 right-0 top-0 bg-cover absolute"
+                     style={{backgroundImage: "url(/login.jpeg)",
+                     width: "1"
+                    }}
                     ></div>
-                </div>
+                </div> */}
                 {/* Login form */}
-                <div className="right flex flex-col justify-evenly">
-                    <div className="text-center py-10">
-
+                <div className=" flex flex-col text-center gap-10 h-full rounded-md">
+                    <div className="m-auto px-4 py-4">
+                       <div>
+                        <h1 className="font-bold text-4xl text-gray-800 font-montserrat"> Login</h1>
+                       </div>
                     </div>
                 </div>
             </div>
