29f39305b7a5604e16716d2961019f6b4df7c689
RiugeBrian <riungebrayandev@gmail.com>
2023-11-03 13:41:08 +0300

    register route

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,5 +1,7 @@
+import { prisma } from "@/util/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
+import bcrypt from "bcryptjs"
 
 const registerUserSchema= z.object({
     name: z.string(),
@@ -12,4 +14,36 @@ export default async function registerUser(
     res: NextApiResponse
 ) {
     const{name, email, password}= registerUserSchema.parse(req.body)
+
+   try {
+     const userExists = await prisma.user.findUnique({
+        where: {email}
+     })
+     //check if user exists
+     if(userExists){
+        return res.status(409).json({message: "User already exists"})
+     }
+
+     //hash the password 
+     const hashedPassword = await bcrypt.hash(password, 10)
+     
+     //create user
+     const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword,
+        }
+     })
+
+     //successful message
+     if(user){
+        return res.status(201).json({message: "User created"})
+     }else{
+        return res.status(500).json({message: "failed to register user"})
+        
+     }
+   } catch (error) {
+    return res.status(500).json({message: "Server error try again later"})
+   }
 }
