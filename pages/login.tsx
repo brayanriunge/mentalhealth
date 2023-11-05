c8a3541abe8536ee646c8d508eadd9daa22262de
RiugeBrian <riungebrayandev@gmail.com>
2023-11-05 19:58:00 +0300

    modifying login

--- pages/login.tsx
+++ pages/login.tsx
@@ -48,7 +48,7 @@ export default function Login(){
 
     return(
         <div className="flex min-h-full bg-blue-400 gap-16 py-10  md:h-full  md:pb-7">
-            <div className=" bg-slate-50 m-auto w-3/5 h-3/4 p-8 my-8 rounded-md">
+            <div className=" bg-slate-50 m-auto w-3/5 h-3/4 p-8 my-8 mb-8 rounded-md">
                 {/* Login form */}
                 <div className=" flex flex-col text-center gap-10 h-full rounded-md">
                     <div className="m-auto px-4 py-4">
