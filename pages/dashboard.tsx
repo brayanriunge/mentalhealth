a7e712095fc9f3859f1adbc57f66610e95799bc3
RiugeBrian <riungebrayandev@gmail.com>
2023-11-07 12:44:00 +0300

    modifying community

---
+++ pages/dashboard.tsx
@@ -0,0 +1,26 @@
+import Layout from "@/components/Layout"
+import MyPost from "@/components/sections/MyPost"
+import { useSession } from "next-auth/react"
+import { redirect } from "next/dist/server/api-utils"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+export default function Dashboard(){
+    const router = useRouter()
+    const{data: session} = useSession()
+    useEffect(()=>{
+        if(!session){
+            router.push("/api/auth/signin")
+        }
+    },[])
+   
+    return(
+        <Layout>
+            <section className=" min-h-full">
+            <h1 className="text-2xl font-bold">welcome back {session?.user?.name}</h1>
+            <MyPost/>
+            </section>
+        </Layout>
+       
+    )
+}
