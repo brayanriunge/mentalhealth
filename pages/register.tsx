e90d403f9b914b91cd040049db74620d12a1a931
RiugeBrian <riungebrayandev@gmail.com>
2023-11-08 21:24:38 +0300

    modifying protection of assessment test

--- pages/register.tsx
+++ pages/register.tsx
@@ -37,7 +37,7 @@ export default function RegisterUser() {
   }
 
   return (
-    <div className="flex  bg-blue-400 gap-16 py-10 md:h-full min-h-full  md:pb-14">
+    <div className="flex  bg-blue-400 gap-16 py-10 md:h-full min-h-full  md:pb-20">
       <div className=" bg-slate-50 m-auto  h-3/4 rounded-md mx-auto p-6  max-w-md w-full">
         {/* Login form */}
         <div className=" flex flex-col text-center gap-10 h-full rounded-md">
